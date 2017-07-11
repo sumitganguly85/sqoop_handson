eddf7f93288f865a7d90f6222fb01e0194a33f1b
sumitganguly85 <sumitganguly85@gmail.com>
2017-07-12 04:08:40 +0530

    Update Sqoop_Hive_Import.sql

--- Sqoop_Hive_Import.sql
+++ Sqoop_Hive_Import.sql
@@ -1,1 +1,2 @@
 sqoop import --connect jdbc:mysql://quickstart.cloudera/retail_db --username root --password cloudera --table departments -m 1 --hive-import
+sqoop create-hive-table  --connect jdbc:mysql://quickstart.cloudera/retail_db --username root --password cloudera --table order_items  --fields-terminated-by ',';
